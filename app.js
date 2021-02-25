7eda92eb2e0cf594aa2c1730b696367475b5512c
Shaun Dawson <iamsdawson@gmail.com>
2021-02-25 18:31:58 -0500

    load event listners, add tasks

--- app.js
+++ app.js
@@ -1,6 +1,49 @@
 // Define UI variables 
-const form = document.querySelector('#task-action');
+const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#tasks');
+const taskInput = document.querySelector('#task');
+
+// Call funtion to load all event listeners
+loadEventListners();
+
+// Load all event listners
+function loadEventListners(){
+  // Add task event
+  form.addEventListener('submit', addTask);  
+}
+
+// Add Task
+function addTask(e){
+  if(taskInput.value === ''){
+    alert('Add a Task');
+  }
+
+  // Create li lelement
+  const li = document.createElement('li');
+  // Add a class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add Class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class = "fa fa-remove"></i>';
+  // Append link to li
+  li.appendChild(link);
+  
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear Input
+  taskInput.value = '';
+
+
+  e.preventDefault();
+}
+
+
+
